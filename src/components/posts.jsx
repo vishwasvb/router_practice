5050906216e9fed7207cfe4cde5986a8343be74a
Vishwas <vishwasvb1997@gmail.com>
2021-04-23 13:54:30 +0530

    queryString

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import queryString from 'query-string';
 
-const Posts = ({match}) => {
+const Posts = ({match,location}) => {
+  // const {sortBy, approved} = queryString.parse(location.search);
+  // console.log(sortBy, approved);
+  const result = queryString.parse(location.search)
+  console.log(result);
   return (
     <div>
       <h1>Posts</h1>
+      {/* Year:{props.match.params.year}, Month:{props.match.params.month} */}
       Year:{match.params.year}, Month:{match.params.month}
     </div>
   );
 };
 
 export default Posts;
+
+
+// npm i query-string@6.1.0
